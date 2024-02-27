bc78e772a5fbabc6bd4dfd45db7a837dbdc8166c
Alain Martini <webmaster@inalto.com>
2024-02-27 18:43:43 +0100

    added enums

--- src/Forms/Components/CoordPicker.php
+++ src/Forms/Components/CoordPicker.php
@@ -6,7 +6,6 @@ use Filament\Forms\Components\Concerns\HasAffixes;
 use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
 use Filament\Forms\Components\Field;
-use Inalto\FilamentCoordPicker\Enums\CoordPickerTheme;
 
 class CoordPicker extends Field
 {
@@ -20,6 +19,8 @@ class CoordPicker extends Field
 
     protected array $config = [];
 
+    protected string $theme = 'default';
+    
     public function getTheme(): string
     {
         return 'default';
