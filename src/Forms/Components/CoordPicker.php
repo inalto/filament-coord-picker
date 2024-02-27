36bcaef528a035c8a452ccdee266b78a9af1ed56
Alain Martini <webmaster@inalto.com>
2024-02-27 18:55:29 +0100

    fixed missing use

--- src/Forms/Components/CoordPicker.php
+++ src/Forms/Components/CoordPicker.php
@@ -6,6 +6,7 @@ use Filament\Forms\Components\Concerns\HasAffixes;
 use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
 use Filament\Forms\Components\Field;
+use Inalto\FilamentCoordPicker\Enums\CoordPickerTheme;
 
 class CoordPicker extends Field
 {
