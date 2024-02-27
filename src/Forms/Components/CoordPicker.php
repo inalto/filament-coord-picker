30983e9733b54bc6e5341743bb6cb3d82c43436f
Alain Martini <webmaster@inalto.com>
2024-02-27 18:35:07 +0100

    gettheme fix

--- src/Forms/Components/CoordPicker.php
+++ src/Forms/Components/CoordPicker.php
@@ -6,6 +6,7 @@ use Filament\Forms\Components\Concerns\HasAffixes;
 use Filament\Forms\Components\Concerns\HasExtraInputAttributes;
 use Filament\Forms\Components\Concerns\HasPlaceholder;
 use Filament\Forms\Components\Field;
+use Inalto\FilamentCoordPicker\Enums\CoordPickerTheme;
 
 class CoordPicker extends Field
 {
@@ -21,7 +22,7 @@ class CoordPicker extends Field
 
     public function getTheme(): string
     {
-        return $this->theme?->value;
+        return 'default';
     }
 
     public function getConfig(): array
